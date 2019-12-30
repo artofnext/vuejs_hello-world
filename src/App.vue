a44e8b1eb7f28755f896fe7afb4d0f2b32167837
artofnext <novnovalex@gmail.com>
2019-12-30 10:55:09 +0200

    add form component, linter no-console

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <hello-world :msg="'Hello World!'"></hello-world>
+    <simple-form :my-array="myArray" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue"
+import SimpleForm from "@/components/SimpleForm.vue"
 
 export default {
-  name: "Vue app",
+  name: "VueApp",
   components: {
     HelloWorld,
-  }
+    SimpleForm,
+  },
+  data: function () {
+    return {
+      myArray: ['dfghfxghbfgh','dfgbfdfgbxdfgb','sdffgdgdsfgh','dfgdfgdfgdfg'],
+    }
+  },
 }
 </script>
 
---
+++ src/components/SimpleForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="form-wrapper">
+        <form name="form" class="form">
+            <input v-model="newValue" type="text" name="input-text" placeholder="Type something...">
+            <button @click.prevent="addToArray">Save!</button>
+        </form>
+        <ul class="list">
+            <li v-for="(value, key) in componentArray" v-bind:key="key">
+                <p>{{ key }}</p> 
+                <p>{{ value }}</p> 
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "simple-form",
+    props: {
+        myArray: Array,
+    },
+    data() {
+        return {
+            newValue: "",
+            componentArray: this.myArray,
+            }
+    },
+    methods: {
+        addToArray() {
+            // let value = this.newValue;
+            console.log('todoArray: ');
+            // console.log(value);
+            this.componentArray.push(this.newValue);
+        },
+    },
+    
+}
+</script>
